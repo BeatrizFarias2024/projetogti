35df7065909cd0e03903d6906180d9443f1d1091
BeatrizFarias2024 <beatrizsantosfarias85@gmail.com>
2025-04-17 14:16:42 -0300

    projetogti

--- routes/web.php
+++ routes/web.php
@@ -2,32 +2,64 @@
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+// Tela de cadastro
 Route::get('/criar-conta', function () {
     return view('criar-conta');
 });
+
+// Salva usuário
 Route::post('/salva-conta', function (Request $request) {
-   // dd($request);
    $user = new User();
    $user->name = $request->name;
    $user->email = $request->email;
    $user->password = $request->password;
+   $user->password = Hash::make($request->password);//senha criptografada
    $user->save();
 
-   return "Usuario salvo com sucesso";
+   return  redirect(route('login')); // redireciona ao login
 })->name('salva-conta');
-//teste
 
+//Tela de login
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+//validação de login
+Route::post('/logar', function (Request $request) {
+    $credentials = $request->validate([
+        'email' => ['required', 'email'],
+        'password' => ['required'],
+    ]);
+
+    if (Auth::attempt($credentials)) {
+        $request->session()->regenerate();
+        return redirect()->intended('dashboard');
+    }
+
+    return back()->withErrors([
+        'email' => 'O email e senha digitados não são válidos',
+    ])->onlyInput('email');
+})->name('logar');
+
+//Dashboard)(pós longin)
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard')->middleware('auth');
+
+// Rotas de teste
 Route::get('/teste', function () {
-    return "o codigo foi testado";
+    return 'O código foi testado';
 });
 
 Route::get('/usuario/{nome}', function ($nome) {
-    return "o usuario atual é:".$nome;
+    return 'o usuário atual é:'.$nome;
 });
 
 Route::get('/soma/{num1}/{num2}', function ($num1,$num2) {
